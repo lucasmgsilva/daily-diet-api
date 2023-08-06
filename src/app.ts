74be8e6425e0592951a7d6da41913ffb93efea4a
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-08-06 16:30:19 -0300

    feat: utilizando apps de autenticação e de refeições

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import { usersRoutes } from './routes/users'
+import { authRoutes } from './routes/auth'
+import { mealsRoutes } from './routes/meals'
 
 export const app = fastify()
 
@@ -11,3 +13,5 @@ app.addHook('preHandler', async (request) => {
 })
 
 app.register(usersRoutes, { prefix: '/users' })
+app.register(authRoutes, { prefix: '/auth' })
+app.register(mealsRoutes, { prefix: '/meals' })
